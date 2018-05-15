fded0ef5d055e99e982e9ee23d2f539e7fb17662
hsinnan75 <arith@iis.sinica.edu.tw>
2018-05-15 15:36:13 +0800

    modified:   GSAlign.cpp

--- GSAlign.cpp
+++ GSAlign.cpp
@@ -658,7 +658,7 @@ void OutputDotplot()
 	outFile = fopen(gpFileName, "w"); DataFileName = (string)OutputPrefix + "." + QueryChrVec[QueryChrIdx].name;
 
 	for (ABiter = AlnBlockVec.begin(); ABiter != AlnBlockVec.end(); ABiter++) if (ABiter->score > 0) vec[ABiter->coor.ChromosomeIdx] += ABiter->score;
-	for (i = 0; i < iChromsomeNum; i++) if (vec[i] >= 1000) ChrScoreVec.push_back(make_pair(i, vec[i])); sort(ChrScoreVec.begin(), ChrScoreVec.end(), CompByChrScore);
+	for (i = 0; i < iChromsomeNum; i++) if (vec[i] >= 10000) ChrScoreVec.push_back(make_pair(i, vec[i])); sort(ChrScoreVec.begin(), ChrScoreVec.end(), CompByChrScore);
 	if (ChrScoreVec.size() > 10) ChrScoreVec.resize(10); thr = ChrScoreVec.rbegin()->second;
 	
 	for (i = 0; i < (int)ChrScoreVec.size(); i++)
